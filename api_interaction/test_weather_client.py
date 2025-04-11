f9c9b241ed30a17fd80bf8560cb4aaed35ccbe72
Human-Gechi <kimberlydaniel363@gmail.com>
2025-04-11 21:44:11 +0100

    made some changes

--- api_interaction/test_weather_client.py
+++ api_interaction/test_weather_client.py
@@ -1,8 +1,9 @@
-import pytest
+import pytest # importing pytest library
+# Adding the parent directory of the package
 from api_interaction.weather_client import user_prompt
 
-# Adding the parent directory of the package
+#checking for valid user prompt
 def test_user_prompt_valid():
      user_prompt()
 def test_user_prompt_invalid():
-     user_prompt()
+     user_prompt() # checking for invalid user prompt
