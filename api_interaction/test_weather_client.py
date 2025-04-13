7728eb9c6e75df5981715491b7f47542eea4a96c
Human-Gechi <kimberlydaniel363@gmail.com>
2025-04-13 16:38:05 +0100

    changes to test file

--- api_interaction/test_weather_client.py
+++ api_interaction/test_weather_client.py
@@ -1,4 +1,9 @@
 import pytest # importing pytest library
+import sys
+import os
+
+# Adding the parent directory of the package
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # Adding the parent directory of the package
 from api_interaction.weather_client import user_prompt
 
